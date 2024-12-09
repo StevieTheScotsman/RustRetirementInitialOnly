a84e6fb902b60bc5c4250057b84d8ebd4d3f4c9f
Steve Curran <steviethescotsman@gmail.com>
2024-12-09 16:32:42 -0600

    run cargo clean for clean up

--- rust-retirement-initial-only/target/debug/deps/rust_retirement_initial_only-735acc63a0db92cb.d
+++
@@ -1,5 +0,0 @@
-/home/steve/source/repos/RustProjects/RustRetirementDec3rd24/RustRetirementInitialOnly/rust-retirement-initial-only/target/debug/deps/librust_retirement_initial_only-735acc63a0db92cb.rmeta: src/main.rs
-
-/home/steve/source/repos/RustProjects/RustRetirementDec3rd24/RustRetirementInitialOnly/rust-retirement-initial-only/target/debug/deps/rust_retirement_initial_only-735acc63a0db92cb.d: src/main.rs
-
-src/main.rs:
